f755cf6f6fbdf0e02b3fb7720095be7281e3395b
Fernando Campione <comm.campione@gmail.com>
2025-03-14 13:42:08 +0000

    add hardcoded url to metadata image

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,11 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: 'One Piece Devil Fruits - by chapter/episode',
   description: 'Explore Devil Fruits from One Piece manga and anime. Search by chapter, episode, or name up to where you are in the series avoiding spoilers.',
-  metadataBase: new URL('https://your-domain.com'),
+  metadataBase: new URL('https://fruitsbychapter.vercel.app'),
   openGraph: {
     title: 'One Piece Devil Fruits Database',
     description: 'Explore Devil Fruits from One Piece manga and anime. Search by chapter, episode, or name up to where you are in the series avoiding spoilers.',
-    url: 'https://your-domain.com',
+    url: 'https://fruitsbychapter.vercel.app',
     siteName: 'One Piece Devil Fruits',
     images: [
       {
